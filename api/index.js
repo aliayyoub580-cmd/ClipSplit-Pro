1bc938f5517c17dd39776baae1ae0aea27b6ed8c
aliayyoub580-cmd <aliayyoub580-cmd@users.noreply.github.com>
2026-07-05 09:41:32 -0700

    Fix Vercel API rewrite

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const app = require("../server/app");
 
 module.exports = function handler(req, res) {
+  const url = new URL(req.url, "http://localhost");
+  const rewrittenPath = url.searchParams.get("path");
+
+  if (rewrittenPath) {
+    url.searchParams.delete("path");
+    const query = url.searchParams.toString();
+    req.url = `/api/${rewrittenPath}${query ? `?${query}` : ""}`;
+  }
+
   if (!req.url.startsWith("/api")) {
     req.url = `/api${req.url.startsWith("/") ? req.url : `/${req.url}`}`;
   }
